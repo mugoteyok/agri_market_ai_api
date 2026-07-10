b60f502f7654a24e0473eb801b3b2a4a9300b2bd
mugoteyok <mugoteyoks@gmail.com>
2026-07-10 18:41:21 +0300

    Enhance Supabase environment variable handling
    Added print statements for debugging Supabase URL and key existence. Added error handling for missing environment variables.

--- database.py
+++ database.py
@@ -7,13 +7,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-SUPABASE_URL = os.getenv(
-    "SUPABASE_URL"
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+
+print("SUPABASE URL:", SUPABASE_URL)
+print(
+    "SUPABASE KEY EXISTS:",
+    bool(SUPABASE_KEY)
 )
 
-SUPABASE_KEY = os.getenv(
-    "SUPABASE_KEY"
-)
+
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise Exception(
+        "Supabase environment variables missing"
+    )
 
 
 supabase: Client = create_client(
